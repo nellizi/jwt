4f14dfd24d9a6977231092313a477e69d568205f
nellizi <masterliz9095@gmail.com>
2022-10-10 02:58:51 +0900

    http bearer방법 설명 주석 추가

--- src/main/java/com/cos/jwt/config/SecurityConfig.java
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-      http.csrf().disable();
+      http.csrf().disable();                                              //STATELESS  == 세션을 사용사지 않겠다.
       http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
               .and()
               .addFilter(corsFilter)  //모든 요청은 이 필터를 타게 된다, CrossOrigin 인증x,시큐리티 필터에 등록 인증o
@@ -34,3 +34,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
 }
+ // .httpBasic().disable() ?
+ // header에 Authorization 키값에 id,pw를 담아서 보내는 방식
+ // 매 요청 시 id,pw를 담고 있기 때문에 쿠키, 세션 필요가 없다. 계속 인증하고 있기 때문에 > 암호화가 안 되는 문제
+ // https 를 쓰면 id,pw가 암호화 되어서 날라간다.
+
+ // Authorization에 토큰을 담는 방법 > 노출은 안 되어야 하지만 id,qw 원문이 아니기 때문에 그나마 좀 낫다 => Bearer 방법
+ // 토큰은 유효시간도 있고 서버에서 재발급 할 수 있다.
+ // bearer 방법을 쓰려면 위에서 다 비활성화 시켜 줘야 함
